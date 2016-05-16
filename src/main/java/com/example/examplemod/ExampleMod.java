9263972125b9b9b22ed60b684e72bb7e53f6c8da
MightyCupcakes <gohydj2003@gmail.com>
2016-05-16 13:56:08 +0800

    My first commit

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.45";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
