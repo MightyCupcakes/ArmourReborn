0f13fa30b496ced791b5e4c8449a7a0e6bfb5e1e
MCHowl <_mattc@hotmail.com>
2016-05-16 14:41:26 +0800

    Hello Jeremy

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,6 +10,7 @@ public class ExampleMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
+    //Hello Jeremy
     
     @EventHandler
     public void init(FMLInitializationEvent event)
