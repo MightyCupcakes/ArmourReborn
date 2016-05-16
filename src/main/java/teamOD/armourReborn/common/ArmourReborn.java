0b50e224b3561a5757bb57e7e7c7f2c15c8ac3b8
MightyCupcakes <gohydj2003@gmail.com>
2016-05-16 15:11:33 +0800

    initial commit

---
+++ src/main/java/teamOD/armourReborn/common/ArmourReborn.java
@@ -0,0 +1,41 @@
+package teamOD.armourReborn.common;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import teamOD.armourReborn.common.lib.LibMisc;
+
+@Mod(modid = LibMisc.MOD_ID, version = LibMisc.VERSION) 
+
+public class ArmourReborn {
+	
+	public static boolean thaumcraftLoaded = false;
+	
+	@Instance(LibMisc.MOD_ID)
+	public static ArmourReborn instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		thaumcraftLoaded = Loader.isModLoaded("Thaumcraft");
+		
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    	
+    }
+}
---
+++ src/main/java/teamOD/armourReborn/common/lib/LibMisc.java
@@ -0,0 +1,22 @@
+package teamOD.armourReborn.common.lib;
+
+public final class LibMisc {
+
+	// Mod Constants
+	public static final String MOD_ID = "ArmorReborn";
+	public static final String MOD_NAME = MOD_ID;
+	public static final String BUILD = "GRADLE:BUILD";
+	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
+	public static final String MC_VERSIONS = "[1.8.8,1.8.9]";
+
+	// Network Contants
+	public static final String NETWORK_CHANNEL = MOD_ID;
+
+	public static final int[] CONTROL_CODE_COLORS = new int[] {
+		0x000000, 0x0000AA, 0x00AA00, 0x00AAAA,
+		0xAA0000, 0xAA00AA, 0xFFAA00, 0xAAAAAA,
+		0x555555, 0x5555FF, 0x55FF55, 0x55FFFF,
+		0xFF5555, 0xFF55FF, 0xFFFF55, 0xFFFFFF
+	};
+
+}
