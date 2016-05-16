be320ef5c0d7a597132e59fb825ce71cf56d9df1
MightyCupcakes <gohydj2003@gmail.com>
2016-05-16 13:51:03 +0800

    my first commit

---
+++ src/main/java/com/example/common/TileMod.java
@@ -0,0 +1,71 @@
+package com.example.common;
+
+/**
+ * A very general class for all TileEntities
+ * @author 
+ *
+ */
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+
+public class TileMod extends TileEntity implements ITickable {
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getBlock() != newState.getBlock();
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound par1nbtTagCompound) {
+		super.writeToNBT(par1nbtTagCompound);
+
+		writeCustomNBT(par1nbtTagCompound);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound par1nbtTagCompound) {
+		super.readFromNBT(par1nbtTagCompound);
+
+		readCustomNBT(par1nbtTagCompound);
+	}
+
+	public void writeCustomNBT(NBTTagCompound cmp) {
+		// NO-OP
+	}
+
+	public void readCustomNBT(NBTTagCompound cmp) {
+		// NO-OP
+	}
+
+	@Override
+	public Packet getDescriptionPacket() {
+		NBTTagCompound nbttagcompound = new NBTTagCompound();
+		writeCustomNBT(nbttagcompound);
+		return new SPacketUpdateTileEntity(pos, -999, nbttagcompound);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+		super.onDataPacket(net, packet);
+		readCustomNBT(packet.getNbtCompound());
+	}
+
+	@Override
+	public final void update() {
+		if (!isInvalid() && worldObj.isBlockLoaded(getPos(), !worldObj.isRemote)) {
+			updateEntity();
+		}
+	}
+
+	protected void updateEntity() {
+		// NO-OP
+	}
+}
